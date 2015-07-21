9a4b290b11e31f06692a41c1c335d9f65e05f6fb
Theo Mills <twmills@twmills.com>
2015-07-21 11:27:20 -0500

    Add default namespace to configuration.

--- lib/hey/configuration.rb
+++ lib/hey/configuration.rb
@@ -1,7 +1,8 @@
 class Hey::Configuration
-  attr_accessor :pubsub_adapter
+  attr_accessor :pubsub_adapter, :namespace
 
   def initialize
     @pubsub_adapter = Hey::Pubsub::Adapters::AsnAdapter
+    @namespace = "pubsub"
   end
 end
